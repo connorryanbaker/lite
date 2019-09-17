b6c1d6a7a58ddf2fbd0c6ff984a9ece5d5403797
connorryanbaker <connorbaker@gmail.com>
2019-09-17 11:38:23 -0400

    commence merge

---
+++ _lite/arlite/searchable.rb
@@ -0,0 +1,13 @@
+require_relative 'db_connection'
+
+module Searchable
+  def where(params)
+    where_line = params.keys.map {|k| "#{k} = ?"}.join(" AND ")
+    res = DBConnection.execute(<<-SQL, *params.values)
+      SELECT *
+      FROM #{self.table_name}
+      WHERE #{where_line}
+    SQL
+    return res[0] ? res.map {|r| self.send(:new, r)} : []
+  end
+end
