b78e37516826f318015d387661d2afe403b93a9b
connorryanbaker <connorbaker@gmail.com>
2019-10-01 14:21:36 -0400

    add minimal js & css

--- _lite/app/public/script.js
+++ _lite/app/public/script.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('js mfs');
+  const todoDescriptions = document.querySelectorAll('.todo-description');
+  const todoTitles = document.querySelectorAll('.todo-title');
+  todoTitles.forEach(el => {
+    el.addEventListener('click', () =>{
+      const id = el.dataset.id;
+      const desc = Array
+                     .from(todoDescriptions)
+                     .filter(el => el.dataset.id === id)[0];
+      desc.classList.toggle('hidden');
+    });
+  });
 });
