45f66f04669f385e9bb767133b5f8d17bdee23bf
connorryanbaker <connorbaker@gmail.com>
2019-10-01 10:14:52 -0400

    add sessions_controller spec

--- _lite/spec/sessions_controller_spec.rb
+++ _lite/spec/sessions_controller_spec.rb
@@ -10,33 +10,52 @@ describe 'SessionsController' do
     let(:req) { Rack::Request.new(env) }
     let(:res) { Rack::Response.new([],200,{}) }
     let(:cr0n) { User.where(username: 'cr0nD0n')[0] }
-    before :each do
-      User.new(username: 'cr0nD0n', password: 'cr0nD0n').save
-      sc = SessionsController.new(req,res,{'username' => 'cr0nD0n',
-                                      'password' => 'cr0nD0n'})
-      sc.invoke_action(:create)
-    end
+    context 'with valid credentials' do
+      before :each do
+        User.new(username: 'cr0nD0n', password: 'cr0nD0n').save
+        sc = SessionsController.new(req,res,{'username' => 'cr0nD0n',
+                                        'password' => 'cr0nD0n'})
+        sc.invoke_action(:create)
+      end
 
-    after :each do
-      User.where(username: 'cr0nD0n')[0].remove
-    end
+      after :each do
+        User.where(username: 'cr0nD0n')[0].remove
+      end
 
-    it 'sets a cookie in the response' do
-      expect(res.headers['Set-Cookie']).to include('_rails_lite_app')
-    end
+      it 'sets a cookie in the response' do
+        expect(res.headers['Set-Cookie']).to include('_rails_lite_app')
+      end
 
-    it 'redirects to /' do
-      expect(res.status).to be(302)
-      expect(res.location).to eq('/')
-    end
+      it 'redirects to /' do
+        expect(res.status).to be(302)
+        expect(res.location).to eq('/')
+      end
 
-    it 'authenticates users, showing them their username once logged in' do
-      token = cr0n.session_token
-      auth_env = {'HTTP_COOKIE' => "_rails_lite_app=%7B%22key%22%3A%22#{token}%22%7D", 'rack.input' => ''}
-      auth_req = Rack::Request.new(auth_env)
-      rc = RootController.new(auth_req, res)
-      rc.invoke_action(:root)
-      expect(res.body.join(" ") =~ /cr0nD0n/).to be_truthy
+      it 'authenticates users, showing them their username once logged in' do
+        token = cr0n.session_token
+        auth_env = {'HTTP_COOKIE' => "_rails_lite_app=%7B%22key%22%3A%22#{token}%22%7D", 'rack.input' => ''}
+        auth_req = Rack::Request.new(auth_env)
+        rc = RootController.new(auth_req, res)
+        rc.invoke_action(:root)
+        expect(res.body.join(" ") =~ /cr0nD0n/).to be_truthy
+      end
+    end 
+    context 'with invalid credentials' do
+
+      before :each do
+        User.new(username: 'cr0nD0n', password: 'cr0nD0n').save
+      end
+      
+      after :each do
+        User.where(username: 'cr0nD0n')[0].remove
+      end
+
+      it 'renders the log in page when bad credentials are submitted' do
+        sc = SessionsController.new(req, res, {'username' => 'cr0nd00n', 'password' => ''})
+        sc.invoke_action(:create)
+        expect(res.status).to be(200)
+        expect(res.body.join(" ") =~ /Sign In/).to be_truthy
+      end
     end
   end
 
